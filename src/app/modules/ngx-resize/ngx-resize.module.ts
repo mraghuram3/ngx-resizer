c55baa1c47ca7af8552237d2fdc5ef61ef0f4fa0
mraghuram3 <mraghuram3@gmail.com>
2018-06-03 15:07:18 +0530

    removed compoent

--- src/app/modules/ngx-resize/ngx-resize.module.ts
+++ src/app/modules/ngx-resize/ngx-resize.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
-import { NgxResizeComponent } from './ngx-resize.component';
-
 import { NgxResizeLeftDirective } from './ngx-resize-left.directive';
 import { NgxResizeRightDirective } from './ngx-resize-right.directive';
 import { NgxResizeTopDirective } from './ngx-resize-top.directive';
@@ -16,14 +14,12 @@ import { NgxResizeDirective } from './ngx-resize.directive';
     CommonModule
   ],
   declarations: [
-    NgxResizeComponent,
     NgxResizeDirective,
     NgxResizeLeftDirective,
     NgxResizeRightDirective,
     NgxResizeTopDirective,
     NgxResizeBottomDirective],
   exports: [
-    NgxResizeComponent,
     NgxResizeDirective,
     NgxResizeLeftDirective,
     NgxResizeRightDirective,
